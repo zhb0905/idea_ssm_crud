a923f220cd89e1796074fddfa0009b9c2ffe4402
zhb <18851822250@163.com>
2022-03-15 20:44:28 +0800

    依赖引入Pom.xml web模块配置web.xml 配置Spring:applicationContext.xml 配置springMVC:spring-mvc.xml 配置mybatis:Mybatis-config.xml 配置mbg:mbg.xml Mbgtest成功生成bean,dao,mapper

---
+++ src/test/java/org/zhb/ssm/crud/test/MBGTest.java
@@ -0,0 +1,27 @@
+package org.zhb.ssm.crud.test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zhb
+ * @create 2022-03-15 16:30
+ */
+public class MBGTest {
+    public static void main(String[] args) throws Exception{
+        List<String> warnings =new ArrayList<>();
+        boolean overwrite = true;
+        File configFile = new File("src/main/resources/mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
