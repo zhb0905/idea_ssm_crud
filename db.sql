b7e35237b3968994d6b982efdd516447a3fa4962
zhb <18851822250@163.com>
2022-03-19 23:16:55 +0800

    zhb:db.sql 在feature-addSqlAndReadme分支中: 添加:db.sql
    +:db.sql

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA `ssm_crud` DEFAULT CHARACTER SET utf8 ;
+
+CREATE TABLE `tbl_dept` (
+                            `dept_id` int NOT NULL AUTO_INCREMENT,
+                            `dept_name` varchar(45) NOT NULL,
+                            PRIMARY KEY (`dept_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `tbl_emp` (
+                           `emp_id` int NOT NULL AUTO_INCREMENT,
+                           `emp_name` varchar(45) NOT NULL,
+                           `gender` char(1) DEFAULT NULL,
+                           `email` varchar(45) DEFAULT NULL,
+                           `d_id` int DEFAULT NULL,
+                           PRIMARY KEY (`emp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
